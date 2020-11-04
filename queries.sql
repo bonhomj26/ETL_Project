7ee745fdcc2c6b1e0adf4bfaf26ff7b3214bbbc6
unknown <bonhomj26@gmail.com>
2020-11-03 20:00:18 -0500

    update

---
+++ queries.sql
@@ -0,0 +1,17 @@
+CREATE TABLE Median_income(
+State VARCHAR,
+"Median income" INT
+);
+
+CREATE TABLE diversityindex(
+"Location" VARCHAR, 
+"Diversity-Index" DEC,
+"Black_or_African_American_alone_percent_2013" DEC,
+"American_Indian_and_Alaska_Native_alone_percent_2013" DEC,
+"Asian_alone_percent_2013"DEC,
+"Native_Hawaiian_and_Other_Pacific_Islander_alone, percent," DEC,
+"Two_or_More_Races_percent_2013" DEC,
+"Hispanic_or_Latino_percent_2003" DEC,
+"White_alone_not_Hispanic_or_Latino_percent_2013" DEC
+);
+
